375a664033db22a11281ab0012f0ab0e549925d0
bartbutenaers <bart.butenaers@gmail.com>
2017-09-09 23:56:00 +0200

    Added window and filters

--- interval_length.js
+++ interval_length.js
@@ -14,12 +14,46 @@
  * limitations under the License.
  **/
  module.exports = function(RED) {
+     "use strict";
     var settings = RED.settings;  
+    const humanizer = require('pretty-ms');
+    const jsonizer = require('parse-ms');
+    
+    function sendMsg(node, msg, interval) {
+        if (interval.millisecs == 0 && node.timeout == false) {
+            // The first message has no interval (since there hasn't been a previous message), so don't send it ...
+            // Except when a timeout is active, then a 0 interval is allowed (at the moment of the timeout).
+            return;
+        }
+        
+        if ((!node.minimum || node.minimum <= interval.millisecs) && (!node.maximum || node.maximum >= interval.millisecs)) {                                      
+            // Convert the array to the specified format
+            switch(node.format) {
+                case 'mills': // milliseconds
+                    msg.payload = interval.millisecs;
+                    break;
+                case 'human': // humanized
+                    msg.payload = humanizer(interval.millisecs);
+                    break;
+                case 'json': // json object
+                    msg.payload = jsonizer(interval.millisecs);
+                    break;
+                default:
+                    // TODO
+            }
+
+            node.send(msg);
+        }
+    }
 
     function IntervalLengthNode(config) {
         RED.nodes.createNode(this,config);
-        this.format = config.format;
-        this.byTopic = config.bytopic; 
+        this.format    = config.format;
+        this.byTopic   = config.bytopic; 
+        this.minimum   = config.minimum;
+        this.maximum   = config.maximum;
+        this.window    = config.window;
+        this.timeout   = config.timeout;
         this.intervals = new Map();
         
         var node = this;
@@ -29,34 +63,67 @@
             
             // When no topic-based resending, store all topics in the map as a single virtual topic (named 'all_topics')
             var topic = node.byTopic ? msg.topic : "all_topics";
-            var previousHrTime = node.intervals.get(topic);
-        
-            if (previousHrTime) {
-                // Calculate the time difference between the previousHrTime and now
-                var difference = process.hrtime(previousHrTime);
+            var interval = node.intervals.get(topic);
+            
+            if (!interval) {
+                interval = { millisecs: 0, hrtime: null, timer: null };
+                node.intervals.set(topic, interval);
+            }
+            
+            if (interval.hrtime) {
+                 // Calculate the time difference between the previous hrTime and now
+                var difference = process.hrtime(interval.hrtime);
                 
                 // The hrtime returns array: the first element contains the number of complete seconds that have elapsed, 
                 // and the second element contains the REMAINING number of nanoseconds.
                 // (see http://tinyexplosions.com/development/2016/03/21/node-timing.html)
                 
-                // Convert the array to the specified format
-                switch(node.format) {
-                    case 'mills': // milliseconds
-                        msg.payload = (difference[0] * 1e9 + difference[1]) / 1e6;
-                        break;
-                    default:
-                        // TODO
+                // Convert the array to milliseconds
+                var milliSeconds = (difference[0] * 1e9 + difference[1]) / 1e6;
+                
+                if (interval.timer) {
+                    // When a window timer is already running (for the specified topic), accumulate the interval to the others
+                    interval.millisecs += milliSeconds;
+                } 
+                else {
+                    interval.millisecs = milliSeconds;
                 }
+            }
+                        
+            if ( node.window > 0 ) {
+                if ( !interval.timer) { 
+                    // Start a new timer, for the specified time window.
+                    // The timer id will be stored, so it can be found when a new msg arrives at the input.
+                    interval.timer = setInterval(function() {        
+                        // At the end of the window, we will send the accumulated interval. 
+                        // When nothing received during the window (sum = 0), nothing will be send unless a timeout is specified. 
+                        if (interval.millisecs > 0 || node.timeout == true) { 
+                            sendMsg(node, msg, interval);           
+                        } 
 
-                node.send(msg);
+                        clearInterval(interval.timer);
+                        interval.timer = null;
+                        interval.millisecs = 0;
+                        
+                    }, node.window); 
+                }
+            } 
+            else {
+                // send the (unaccumulated) interval length 
+                sendMsg(node, msg, interval);
             }
             
-            node.intervals.set(topic, newHrTime);
+            // Store the time when this message has arrived 
+            interval.hrtime = newHrTime;
         });
         
         node.on("close", function() {
-            node.statistics.clear();
+            node.intervals.clear();
             node.status({});
+            
+            for(var interval of node.intervals.values()) {
+                clearInterval(interval.timer);
+            }
         });
     }
 
